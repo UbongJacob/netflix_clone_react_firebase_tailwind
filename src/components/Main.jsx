81eb93e5c6646a921ef61086aab36e1704e29ada
Ubong Jacob <jacobubong12@gmail.com>
2022-09-04 17:13:09 +0100

    Done with the hero section

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -11,6 +11,10 @@ const Main = () => {
   }, []);
 
   const movie = movies[Math.floor(Math.random() * movies.length)];
+  const truncateString = (str, num) => {
+    if (str?.length > num) return str.slice(0, num) + '...';
+    else return str;
+  };
 
   return (
     <div className='w-full h-[550px] text-white'>
@@ -21,9 +25,9 @@ const Main = () => {
           src={`${requests.imgBaseUrl}${movie?.backdrop_path}`}
           alt={movie?.title}
         />
-
         <div className='absolute w-full top-[20%] p-4 md:p-8'>
-          <div>
+          <h1 className='text-3xl md:text-5xl font-bold'>{movie?.title}</h1>
+          <div className='my-4'>
             <button className='border bg-gray-300 text-black border-gray-300 py-2 px-5'>
               Play
             </button>
@@ -31,6 +35,12 @@ const Main = () => {
               Watch Later
             </button>
           </div>
+          <p className='text-gray-400 text-sm'>
+            Released: {movie?.release_date}
+          </p>
+          <p className='w-full md:max-w-[70%] lg:max-w-[50%] xl:max-w-[35%] text-gray-200'>
+            {truncateString(movie?.overview, 300)}
+          </p>
         </div>
       </div>
     </div>
