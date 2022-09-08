b8d3691a68c63f09bcb98db7b4d8102bede2f42b
Ubong Jacob <ubongjacob13@gmail.com>
2022-09-08 11:22:50 +0100

    added mobile responsiveness

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -17,7 +17,7 @@ const Navbar = () => {
   return (
     <div className='flex items-center justify-between p-4 z-[100] w-full absolute'>
       <Link to='/'>
-        <h1 className='text-red-600 text-4xl font-bold  cursor-pointer'>
+        <h1 className='text-red-600  md:text-2xl lg:text-4xl font-bold  cursor-pointer'>
           DevUby NETFLIX CLONE
         </h1>
       </Link>
@@ -29,7 +29,7 @@ const Navbar = () => {
 
           <button
             onClick={handleLogout}
-            className='bg-red-600 px-6 py-4 rounded cursor-pointer text-white'
+            className='bg-red-600 px-3 py-2 md:px-4 md:py-3 lg:px-6 lg:py-4 rounded cursor-pointer text-white'
           >
             Logout
           </button>
@@ -37,10 +37,12 @@ const Navbar = () => {
       ) : (
         <div>
           <Link to='/login'>
-            <button className='text-white pr-4'>Sign In </button>
+            <button className='text-white px-3 py-2 md:px-4 md:py-3 pr-4'>
+              Sign In{' '}
+            </button>
           </Link>
           <Link to='/signup'>
-            <button className='bg-red-600 px-6 py-4 rounded cursor-pointer text-white'>
+            <button className='bg-red-600 px-3 py-2 md:px-4 md:py-3 lg:px-6 lg:py-4 rounded cursor-pointer text-white'>
               Sign Up
             </button>
           </Link>
